981088154956de69fe37ccf8b4bfac209ae6a831
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-10 11:32:16 +0100

    More templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,17 @@
-from django.template import Context, loader
+#from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
-from django.http import HttpResponse
+from django.http import Http404
+#from django.http import HttpResponse
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    # is Context a object which is passed a dict as an argument?
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-        'user': 'PxG',
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+	# shorthand helper function
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
