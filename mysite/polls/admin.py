04a90db9364efa87379a59c19c0831e4d7c5fc94
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-10 09:40:46 +0100

    more cms mods

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from polls.models import Poll, Choice
 
-class ChoiceInline(admin.StackedInline):
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -12,6 +13,11 @@ class PollAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    #list_display = ('question', 'pub_date')
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
 
 admin.site.register(Poll, PollAdmin)
 #admin.site.register(Choice)
