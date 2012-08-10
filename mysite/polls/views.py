604a449cede4190f0abafdae91db4f28663ac4fd
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-10 15:17:17 +0100

    Redundant view functions removed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,19 +4,6 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from polls.models import Choice, Poll
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    # shorthand helper function
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p},
-                               context_instance=RequestContext(request))
-
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
