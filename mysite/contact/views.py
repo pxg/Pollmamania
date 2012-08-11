a10ff3f9292beffbcf199812c6bd77d06449c3d9
Pete Graham <petegraham1@gmail.com>
2012-08-11 18:54:23 +0100

    Form hooked up with Validation

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render_to_response
 from forms import ContactForm
 
 def index(request):
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+    else:
+        form = ContactForm()
     return render_to_response('contact/index.html', {'form': form}, context_instance=RequestContext(request))
