decbc6955dc7ca78176001039b5dbe01b560d49c
Pete Graham <petegraham1@gmail.com>
2012-08-11 18:43:15 +0100

    Feedback scaffold setup

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
-	print 'got here'
-	return render_to_response('contact/index.html')
+	#print 'got here'
+	return render_to_response('contact/index.html', {}, context_instance=RequestContext(request))
