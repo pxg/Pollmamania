cfb58a751c5911aa1bc06417fd6d83647e19c96f
Pete Graham <petegraham1@gmail.com>
2012-08-11 18:50:59 +0100

    Form displaying

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from forms import ContactForm
 
 def index(request):
-	#print 'got here'
-	return render_to_response('contact/index.html', {}, context_instance=RequestContext(request))
+    form = ContactForm()
+    return render_to_response('contact/index.html', {'form': form}, context_instance=RequestContext(request))
